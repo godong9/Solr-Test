63b21f37382acb5a8245f6d04d9f872e0d6690ad
Donghyun Go <godong9@gmail.com>
2016-02-17 16:00:54 +0900

    Update solr

--- reset_of_dev.sh
+++ reset_of_dev.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-#rm -rf dump_backup
-#mv dump dump_backup
-#mongodump -d deployd -c notes
-
-./solr-5.4.1/bin/solr stop -all
+./stop_solr.sh
 pkill mongo-connector
-
-#mongo drop_notes.js
 
 rm -rf ./solr-5.4.1/server/solr/notes/data
 
@@ -19,7 +13,4 @@ rm logs/mongo-connector.log
 sleep 5;
 
 ./dev_start_mongo_connector.sh
-
-#mongorestore -d deployd -c notes dump/deployd/notes.bson
-
 
--- restart_of_dev.sh
+++ restart_of_dev.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-#rm -rf dump_backup
-#mv dump dump_backup
-#mongodump -d deployd -c notes
-
-./solr-5.4.1/bin/solr stop -all
+./stop_solr.sh
 pkill mongo-connector
-
-#mongo drop_notes.js
-
-#rm -rf ./solr-5.4.1/server/solr/notes/data
 
 ./start_solr.sh
 
@@ -19,7 +11,5 @@ rm logs/mongo-connector.log
 sleep 5;
 
 ./dev_start_mongo_connector.sh
-
-#mongorestore -d deployd -c notes dump/deployd/notes.bson
 
 
